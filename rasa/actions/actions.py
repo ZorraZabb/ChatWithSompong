f3a309b49117e79a402ac7ae4b743ca00e8e2b1e
Sorrasub <gagpoohpooh@gmail.com>
2023-03-06 16:20:44 +0700

    Filter Curse word

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +25,25 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionFilterCurseWords(Action):
+    def name(self) -> Text:
+        return "action_filter_curse_words"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # List of curse words
+        curse_words = ["fuck", "shit", "bitch"]
+        
+        # Get the latest user message
+        latest_message = tracker.latest_message.get('text', '').lower()
+        
+        # Check if any curse words are in the latest message
+        if any(word in latest_message for word in curse_words):
+            # Warn the user
+            dispatcher.utter_message(text="Please refrain from using offensive language.")
+        
+        return []
+
